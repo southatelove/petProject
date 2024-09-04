02a76fcbc053b907d626c8502335259b167b62f9
southatelove <artem.jlez@gmail.com>
2024-09-04 14:28:46 +0300

    update: Добавление Темной и светлой темы через контекст, а также создание кастомного хука для получения и изменения темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Link, Route, Routes } from "react-router-dom";
 
-import "./index.scss";
+import "./styles/index.scss";
 
-import { AboutPageAsync } from "./Pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./Pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense, useContext, useState } from "react";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 export default function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
 
