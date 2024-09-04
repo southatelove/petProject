a908bf2e8460d70185d79c3aedfe99b77d93a4e2
southatelove <artem.jlez@gmail.com>
2024-09-04 11:59:32 +0300

    update/добавление lazyloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Counter } from "./components/Counter";
+import { Link, Route, Routes } from "react-router-dom";
+
 import "./index.scss";
+
+import { AboutPageAsync } from "./Pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./Pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <div className="app">
-      asdasdasd
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
