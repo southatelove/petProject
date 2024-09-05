6e269928145e512f3ab2b7e392ee4673d0f95960
southatelove <artem.jlez@gmail.com>
2024-09-05 15:14:57 +0300

    update: добавление абсолютных импортов через index.ts, переход от молекулярной архитектуры к FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
-import { Link, Route, Routes } from "react-router-dom";
-
 import "./styles/index.scss";
 
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Link, Route, Routes } from "react-router-dom";
+
+import { useTheme } from "./providers/ThemeProvider";
+
 import { Suspense } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
@@ -24,8 +25,8 @@ export default function App() {
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
