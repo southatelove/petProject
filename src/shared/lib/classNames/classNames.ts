10cfce7287de61078bd129ff6d9e1d26742cf6f4
southatelove <artem.jlez@gmail.com>
2024-09-08 18:16:31 +0300

    update: добавление первых тестов (jest) для кастомной lib classNames

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,18 +4,14 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  mods?: Mods,
-  additional?: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     cls,
-    ...additional,
+    ...additional.filter(Boolean),
     ...Object.entries(mods)
-      .filter(([className, value]) => Boolean(value))
+      .filter(([_, value]) => Boolean(value))
       .map(([className]) => className),
   ].join(" ");
 }
-
-classNames("remove-btn", { hovered: true, selectable: true, red: false }, [
-  "pdg",
-]);
