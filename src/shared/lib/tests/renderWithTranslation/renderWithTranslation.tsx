314fa9d034e771f940244807ecb64b1dff062299
southatelove <artem.jlez@gmail.com>
2024-11-18 14:55:16 +0300

    update: fix i18 tests

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -2,10 +2,10 @@
 
 import { render } from "@testing-library/react";
 import { I18nextProvider } from "react-i18next";
-import i18nextForTests from "shared/config/i18n/i18nextForTests";
+import i18nForTests from "shared/config/i18n/i18nForTests";
 
 export function renderWithTranslation(component: React.ReactNode) {
   return render(
-    <I18nextProvider i18n={i18nextForTests}>{component}</I18nextProvider>
+    <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
   );
 }
