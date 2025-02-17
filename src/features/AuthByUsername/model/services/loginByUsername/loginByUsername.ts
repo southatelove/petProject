07cbcc826c843e7d6cd39f2f66fc44ce85b88bc7
southatelove <artem.jlez@gmail.com>
2025-02-17 11:55:36 +0300

    update: добавление unit тестов для getLogin selectors, создание своего TestAsyncThunk для тестирования ReduxAsynkThunk

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { userActions } from "entities/User";
 import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
 
-interface LoginByUsernameProps {
+export interface LoginByUsernameProps {
   username: string;
   password: string;
 }
